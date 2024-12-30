71a3f6dccb55883e642bce619eafe6107908bf57
Anıl Soylu <anil.soylu94@gmail.com>
2024-12-30 06:05:30 +0300

    updated: fixed ui

--- components/shared/home/left-info/detail-item.tsx
+++ components/shared/home/left-info/detail-item.tsx
@@ -2,10 +2,7 @@ import { DetailItemProps } from "@/types/information"
 import { memo } from "react"
 
 const DetailItem = memo(({ detail }: DetailItemProps) => (
-  <div
-    className="items-center flex flex-row justify-between py-2.5
-                  hover:bg-black/5 transition-colors duration-200 group"
-  >
+  <div className="items-center flex flex-row justify-between py-2.5 hover:bg-black/5 transition-colors duration-200 group">
     <dt className="text-black text-xs font-bold uppercase group-hover:scale-105 transition-transform duration-200">
       {detail.title}
     </dt>
