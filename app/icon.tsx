e8770e3985fb24961863aff6394f658693e8a9da
Anıl Soylu <56033298+anilsoylu@users.noreply.github.com>
2025-02-01 07:53:45 +0300

    updated: icon optimization

--- app/icon.tsx
+++ app/icon.tsx
@@ -1,10 +1,17 @@
 import { ImageResponse } from "next/og"
 
-export const size = {
+interface IconSize {
+  width: number
+  height: number
+}
+
+const ICON_SIZE: IconSize = {
   width: 32,
   height: 32,
-}
-export const contentType = "image/png"
+} as const
+
+export const size = ICON_SIZE
+export const contentType = "image/png" as const
 
 export default function Icon() {
   return new ImageResponse(
@@ -14,9 +21,17 @@ export default function Icon() {
           display: "flex",
           width: "100%",
           height: "100%",
+          alignItems: "center",
+          justifyContent: "center",
         }}
       >
-        <svg viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg" fill="#000">
+        <svg
+          viewBox="0 0 32 32"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="currentColor"
+          width={ICON_SIZE.width}
+          height={ICON_SIZE.height}
+        >
           <path
             d="M 0 16 C 0 4, 4 0, 16 0 S 32 4, 32 16, 28 32 16 32, 0 28, 0 16"
             transform="rotate(0,16,16) translate(0,0)"
